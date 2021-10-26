7f2c9cca9b978f115f69c4b4a0ae66c19c3aca30
林和强 <45845311@qq.com>
2021-10-26 17:47:36 +0800

    add markdown

--- code/05/helloWorld/static/main.js
+++ code/05/helloWorld/static/main.js
@@ -1,6 +1,6 @@
 //外面这层是等html元素加载完成,否则js获取不到元素节点
 document.addEventListener('DOMContentLoaded', function () {
-    var text = document.getElementByClassName('text'); //获取到class name为text的节点
+    var text = document.getElementsByClassName('text'); //获取到class name为text的节点
 
     // 给获取到的第一个节点安排一个事,就是告诉它有人点击了就把这个节点里面的字改一下.
     // js中这种行为成为绑定事件,右边的function就是事件触发后要处理的东西,可以称为事件回调函数
